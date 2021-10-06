269e0e71a5d386cd3274a5df848d29ff1e716508
Max Harraß <92017378+Robocopmax@users.noreply.github.com>
2021-10-06 13:56:12 +0200

    added first Test and needed lib

---
+++ src/de/arvato/application/Aufgabe3Test.java
@@ -0,0 +1,20 @@
+package de.arvato.application;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class Aufgabe3Test {
+
+    @Before
+    public void setup(){
+        Aufgabe3 a = new Aufgabe3();
+        
+        a.getOptimalValue(money, flears);
+    }
+        
+    @Test
+    public void test() {
+        
+    }
+}
+    
